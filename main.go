d5d9e2d0330592ce86942e0250b89a1e3e95e663
flogit2161 <flo12gab@gmail.com>
2026-01-22 02:59:54 +0100

    MiddleWare Hits & First handlers added

--- main.go
+++ main.go
@@ -1,12 +1,52 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+
+	count := cfg.fileserverHits.Load()
+	w.Write([]byte(fmt.Sprintf("Hits: %v", count)))
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	cfg.fileserverHits.Store(0)
+	w.Write([]byte("Reset number of hits to server"))
+}
+
 func main() {
+	apiCfg := &apiConfig{}
 	serveMux := http.NewServeMux()
+	fileServer := http.FileServer(http.Dir("."))
+	handler := http.StripPrefix("/app", fileServer)
+	serveMux.Handle("/app/", apiCfg.middlewareMetricsInc(handler))
+	serveMux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+	serveMux.HandleFunc("/metrics", apiCfg.handlerMetrics)
+	serveMux.HandleFunc("/reset", apiCfg.handlerReset)
+
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: serveMux,
